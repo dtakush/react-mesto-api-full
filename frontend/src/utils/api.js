157ffd841cd75234ecca0849522af1c6e85f9303
Daria Takush <dashatakush@yandex.ru>
2021-08-06 16:55:18 +0300

    15sprint/devepop/1.3

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -5,11 +5,19 @@ class Api {
       this.headers = headers;
     }
 
+    _getHeaders(){
+      const token = localStorage.getItem('jwt'); 
+      return {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      }
+    }
+
     //Запрос карточек с сервера
     getInitialCards() {
       return fetch(`${this.baseUrl}/cards`, {
         method: 'GET',
-        headers: this.headers
+        headers: this._getHeaders()
       })
       .then(this._checkResponse)
     }
@@ -18,7 +26,7 @@ class Api {
     addCard(card) {
       return fetch(`${this.baseUrl}/cards`, {
         method: 'POST',
-        headers: this.headers,
+        headers: this._getHeaders(),
         body: JSON.stringify({
           link: card.link,
           name: card.name,
@@ -31,7 +39,7 @@ class Api {
      getUserInfo() {
       return fetch(`${this.baseUrl}/users/me`, {
         method: 'GET',
-        headers: this.headers
+        headers: this._getHeaders()
       })
       .then(this._checkResponse)
     }
@@ -40,7 +48,7 @@ class Api {
     setUserInfo(items) {
       return fetch(`${this.baseUrl}/users/me`, {
         method: 'PATCH',
-        headers: this.headers,
+        headers: this._getHeaders(),
         body: JSON.stringify({
           name: items.name,
           about: items.about
@@ -53,7 +61,7 @@ class Api {
     deleteCard(id) {
       return fetch(`${this.baseUrl}/cards/${id}`, {
         method: 'DELETE',
-        headers: this.headers
+        headers: this._getHeaders()
       })
       .then(this._checkResponse)
     }
@@ -62,7 +70,7 @@ class Api {
      changeLikeCardStatus(id, isLiked) {
       return fetch(`${this.baseUrl}/cards/likes/${id}`, {
         method: isLiked ? 'PUT' : 'DELETE',
-        headers: this.headers
+        headers: this._getHeaders()
 
       })
       .then(this._checkResponse)
@@ -72,7 +80,7 @@ class Api {
     setNewAvatar(avatar) {
       return fetch(`${this.baseUrl}/users/me/avatar`, {
         method: 'PATCH',
-        headers: this.headers,
+        headers: this._getHeaders(),
         body: JSON.stringify({
           avatar: avatar.avatar,
         })
@@ -88,14 +96,13 @@ class Api {
     }
     }
 
-
 const api = new Api({
       baseUrl: 'https://api.dtakush.mesto.student.nomoredomains.club',
-      headers: {
+      /* headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-      }
+      } */
   });
 
 
