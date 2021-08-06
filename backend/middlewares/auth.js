b1f4f5b0f9e55b9a6cecc8c0e14f62c4aa147180
Daria Takush <dashatakush@yandex.ru>
2021-08-06 18:55:04 +0300

    15sprint/devepop/1.5

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     throw new Unauthorized('Необходима авторизация');
   }
