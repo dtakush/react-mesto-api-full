682d96c150a4b23f4f0732be92077d9d3d075ba9
Daria Takush <dashatakush@yandex.ru>
2021-08-06 18:04:22 +0300

    15sprint/devepop/1.5

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -106,10 +106,11 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
+      console.log(token);
       const token = jwt.sign({ _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret',
         { expiresIn: '7d' });
-      res.status(200).send({ token });
+      res.send({ token });
     })
     .catch(next);
 };
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
     req.user = payload;
     next();
   } catch (err) {
