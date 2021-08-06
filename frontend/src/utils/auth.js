aaf36dcbe6e1c4a0afa866d92955651e08ded74a
Daria Takush <dashatakush@yandex.ru>
2021-08-06 15:00:21 +0300

    15sprint/devepop/1.2

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -52,6 +52,7 @@ export const authorize = (email, password) => {
         }
     })
     .then((data) => {
+      console.log(data);
       if (data.token) {
         localStorage.setItem('jwt', data.token);
         return data;
